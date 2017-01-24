c7660019700d91b681a9f4651e339fc8cf2d932d
rnb14 <rnb14@ic.ac.uk>
2017-01-24 22:33:43 +0000

    Added fixed-point.h Starting to add macros

---
+++ src/threads/fixed-point.h
@@ -0,0 +1,13 @@
+#ifndef THREADS_FIXED_POINT_H
+#define THREADs_FIXED_POINT_H
+
+#include <stdint.h>
+
+/*Macros for 17.14 fixed-point number representation*/
+
+#define P 17
+
+#define Q 14
+
+
+#endif /* threads/fixed-point.h */
