8d621440fa1faa2ba4b08ca2acb3d32df1d17a64
rnb14 <rnb14@imperial.ac.uk>
2017-02-08 15:41:19 +0000

    Implemented basic user memory access -Added get_user and put_user as described in specs section 4.1.5 -temporary modification to setup_stack() to begin argument passing -modified page_fault() as described in section 4.1.5

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -437,7 +437,10 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+        /* Temporary change to PHYS_BASE - 12.
+           Will work for test programs that does not examine 
+           arguments */
+        *esp = PHYS_BASE - 12;
       else
         palloc_free_page (kpage);
     }
--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,10 +1,14 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include "threads/vaddr.h"
+#include <inttypes.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+static int get_user (const uint8_t *uaddr);
+static bool put_user (uint8_t *udst, uint8_t byte);
 
 void
 syscall_init (void) 
@@ -15,6 +19,32 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
+  uint32_t sys_call_number = *((uint32_t*) f->esp);
+  printf ("system call number %"PRIu32"!\n", sys_call_number);
   thread_exit ();
+}
+
+/* Reads a byte at user virtual address UADDR.
+   UADDR must be below PHYS_BASE.
+   Returns the byte value if successful, -1 if a segfault
+   occurred. */
+static int
+get_user (const uint8_t *uaddr)
+{
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+       : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+
+/* Writes BYTE to user address UDST.
+UDST must be below PHYS_BASE.
+Returns true if successful, false if a segfault occurred. */
+static bool
+put_user (uint8_t *udst, uint8_t byte)
+{
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+       : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+  return error_code != -1;
 }
