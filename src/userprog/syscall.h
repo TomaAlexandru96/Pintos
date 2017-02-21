bce0a42953158706756d115cae9a92dacf5a7a81
rnb14 <rnb14@imperial.ac.uk>
2017-02-21 01:08:19 +0000

    Fixed exception handling but broke some other tests:dont know why yet

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,11 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include "threads/interrupt.h"
+
 void syscall_init (void);
+
+/* to test memory in exception.c */
+void syscall_exit_t (struct intr_frame *f, int status);
 
 #endif /* userprog/syscall.h */
