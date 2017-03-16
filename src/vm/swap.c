e6b00025c92c469204df49d5d797677b0cb959f8
mihaivanea <mihaivanea1@gmail.com>
2017-03-16 13:07:11 +0000

    implemented swap slot insert and reclaim, needs testing

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -4,21 +4,22 @@ static struct lock swap_lock;
 static struct block *swap_block;
 static struct bitmap *slots_map;
 static struct hash swap_table;
+#define SWAP_SIZE(swap_block) ((uint32_t) block_size ((struct block *) swap_block))
+
 
 void
 swap_init(void)
 {
   swap_block = block_get_role (BLOCK_SWAP);
-  swap_size = block_size (swap_block);
-  slots_map = bitmap_create (swap_size / PGSIZE);
+  slots_map = bitmap_create (SWAP_SIZE (swap_block) / PGSIZE);
   lock_init (&swap_lock);
-  hash_init (&swap_table);
+  hash_init (&swap_table, &swap_hash_func, &swap_less_func, NULL);
 }
 
 unsigned
 swap_hash_func (const struct hash_elem *e, void *aux UNUSED)
 {
-  return (uint32_t) (hash_entry (e, struct swap_table_entry, hash_elem)->pg_addr);
+  return (uint32_t) (hash_entry (e, struct swap_table_entry, hash_elem)->addr);
 }
 
 bool
@@ -26,16 +27,18 @@ swap_less_func (const struct hash_elem *a,
                 const struct hash_elem *b,
                 void *aux UNUSED)
 {
-  uint32_t a_el = (uint32_t) (hash_entry (a, struct swap_table_entry, hash_elem)->pg_addr);
-  uint32_t b_el = (uint32_t) (hash_entry (b, struct swap_table_entry, hash_elem)->pg_addr);
+  uint32_t a_el = (uint32_t) (hash_entry (a, struct swap_table_entry,
+                                          hash_elem)->addr);
+  uint32_t b_el = (uint32_t) (hash_entry (b, struct swap_table_entry,
+                                          hash_elem)->addr);
 
   return a_el < b_el;
 }
 
-int
-get_free_slot ()
+static int
+get_free_slot (void)
 {
-  int slot_size = swap_size / PGSIZE;
+  int slot_size = SWAP_SIZE (swap_block) / PGSIZE;
   bool swap_full = bitmap_all (slots_map, 0, slot_size);
   int start = 0;
   if (!swap_full)
@@ -55,43 +58,62 @@ insert_swap_slot (void *pg_addr)
   lock_acquire (&swap_lock);
   struct swap_table_entry *new_entry = (struct swap_table_entry *) malloc
                                        (sizeof (struct swap_table_entry));
+  if (new_entry == NULL)
+    {
+      PANIC ("Malloc failed!");
+    }
   int start = get_free_slot ();
   new_entry->addr = pg_addr;
   new_entry->sector = start;
   hash_insert (&swap_table, &new_entry->hash_elem);
 
+  bitmap_set (slots_map, start, true);
   for (int i = 0; i < SWAP_SLOT_SIZE; i++) {
-    swap_block = block_write (block, start, (const void *) pg_addr);
-    bitmap_set (slots_map, start, true);
+    block_write (swap_block, start, (const void *) pg_addr);
     pg_addr += BLOCK_SECTOR_SIZE;
     start++;
   }
   lock_release (&swap_lock);
-  return swap_block;
 }
 
 struct block_sector_t *
 reclaim_swap_slot (void *pg_addr)
 {
   lock_acquire (&swap_lock);
-  //
-  // struct swap_table_entry search;
-  // search->addr = pg_addr;
-  //
-  //
-  //
-  //
-  // struct hash_elem *elem = hash_find (&swap_table, &search->hash_elem);
-  // if (elem == NULL)
-  //   {
-  //     return NULL
-  //   }
-  // 
-  // for (int i = 0; i < SWAP_SLOT_SIZE; i++)
-  //   {
-  //     bitmap_scan_and_flip (&swap_table, 0, SWAP_SLOT_SIZE, false);
-  //   }
+  struct swap_table_entry search;
+  search.addr = pg_addr;
+  struct hash_elem *elem = hash_delete (&swap_table, &search.hash_elem);
 
+
+
+  if (elem == NULL)
+    {
+      // ERROR: not in swap
+      return NULL;
+    }
+
+  struct swap_table_entry *reclaim_elem = hash_entry (elem,
+                                          struct swap_table_entry, hash_elem);
+
+  for (int i = 0; i < SWAP_SLOT_SIZE; i++)
+    {
+      bitmap_scan_and_flip (slots_map, (int) reclaim_elem->sector,
+                                        SWAP_SLOT_SIZE, false);
+    }
+
+  void *reclaim_page = malloc (PGSIZE);
+  if (reclaim_page == NULL)
+    {
+      PANIC ("Malloc failed!");
+    }
+
+  for (int i = 0; i < SWAP_SLOT_SIZE; i++)
+    {
+      block_read (swap_block, (int) reclaim_elem->sector + i, reclaim_page);
+      reclaim_page += SWAP_SLOT_SIZE;
+    }
+
+  free (reclaim_elem);
   lock_release (&swap_lock);
-  return NULL;
+  return reclaim_page;
 }
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,5 +1,5 @@
-#ifndef VM_PAGE_H
-#define VM_PAGE_H
+#ifndef VM_SWAP_H
+#define VM_SWAP_H
 
 #include <stdbool.h>
 #include "threads/thread.h"
@@ -10,20 +10,24 @@
 #include "threads/thread.h"
 #include "devices/block.h"
 #include "lib/kernel/bitmap.h"
+#include "threads/vaddr.h"
 
 /* The number of bloack sectors needed to store on page*/
 #define SWAP_SLOT_SIZE 8
 
 hash_hash_func swap_hash_func;
 hash_less_func swap_less_func;
+static int get_free_slot (void);
 
 struct swap_table_entry
 {
   struct hash_elem hash_elem;
   void *addr;
-  block_sector_t *sector;
+  block_sector_t sector;
 };
 
 void swap_init(void);
 struct block_sector_t *reclaim_swap_slot (void *);
-struct block_sector_t *insert_swap_slot (void *);
+void insert_swap_slot (void *);
+
+#endif
